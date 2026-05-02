2ef95731d0572574eb287a315e97716d1a44f86e
Boston <bostonrohan@gmail.com>
2026-05-02 11:41:22 -0400

    feat: update rhizome page with carousel arrows outside image, header image in carousel

--- src/components/photo-carousel.tsx
+++ src/components/photo-carousel.tsx
@@ -14,22 +14,24 @@ export default function PhotoCarousel({
   images: { src: string; alt: string }[];
 }) {
   return (
-    <Carousel className="w-full">
-      <CarouselContent className="w-full">
-        {images.map(({ src, alt }, index) => (
-          <CarouselItem key={index}>
-            <div className="flex items-center justify-center w-full h-full max-w-3xl mx-auto">
-              <img
-                src={src}
-                alt={alt}
-                className="object-cover rounded-2xl w-full h-full aspect-[4/3]"
-              />
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
+    <Carousel className="w-full max-w-2xl mx-auto">
+      <div className="relative">
+        <CarouselContent>
+          {images.map(({ src, alt }, index) => (
+            <CarouselItem key={index}>
+              <div className="flex items-center justify-center w-full h-full">
+                <img
+                  src={src}
+                  alt={alt}
+                  className="object-cover rounded-2xl w-full aspect-[4/3]"
+                />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="absolute -left-10 top-1/2 -translate-y-1/2" />
+        <CarouselNext className="absolute -right-10 top-1/2 -translate-y-1/2" />
+      </div>
     </Carousel>
   );
 }
